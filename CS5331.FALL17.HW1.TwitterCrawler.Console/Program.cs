3c0a9e7ad85e3073156861c2947db067414055a5
Christopher McMillon <Christopher McMillon>
2017-09-10 17:14:14 -0500

    Add project files.

---
+++ CS5331.FALL17.HW1.TwitterCrawler.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS5331.FALL17.HW1.TwitterCrawler.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Herro World");
+            Console.ReadLine();
+        }
+    }
+}
